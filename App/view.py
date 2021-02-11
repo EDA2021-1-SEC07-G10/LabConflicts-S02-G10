2e7b292ba076bc4ea8a3e4189e0683832b694f6d
m-almeci <m.almeciga@uniandes.edu.co>
2021-02-10 19:26:03 -0500

    cambios en main

--- App/view.py
+++ App/view.py
@@ -78,7 +78,7 @@ while True:
         print('Total de libros cargados: ' + str(lt.size(books)))
 
         # TO-DO: Modificación de Est-1 en el Lab 2
-
+        print("3- Cargar Book-Tags")
 
         # TO-DO: Modificación de Est-2 en el Lab 2
 
